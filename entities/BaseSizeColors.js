e291f7d5ddcada33ad14d564b765bd2a7dd65087
veera <vreddy@jazeeratechnologies.com>
2021-12-03 16:24:55 +0530

    1.0.26

--- entities/BaseSizeColors.js
+++ entities/BaseSizeColors.js
@@ -155,7 +155,7 @@ var BaseSizeColors = /** @class */ (function () {
         __metadata("design:type", BasesMaster_1.BasesMaster)
     ], BaseSizeColors.prototype, "base", void 0);
     __decorate([
-        typeorm_1.JoinColumn({ name: "system_code", referencedColumnName: "systemCode" }),
+        typeorm_1.JoinColumn({ name: "system_code", referencedColumnName: "system_code" }),
         typeorm_1.ManyToOne(function (type) { return DatabaseMaster_1.DatabaseMaster; }, function (db) { return db.system_code; }),
         __metadata("design:type", DatabaseMaster_1.DatabaseMaster)
     ], BaseSizeColors.prototype, "dbMaster", void 0);
